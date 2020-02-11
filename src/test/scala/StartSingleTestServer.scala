add328fc584b21ea640d35e7737bed73131abc6d
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2020-02-11 20:26:39 +0800

    perfect & weird readFile()

--- src/test/scala/StartSingleTestServer.scala
+++ src/test/scala/StartSingleTestServer.scala
@@ -1,11 +1,18 @@
-import cn.graiph.regionfs.shell.{ShellNodeServerStarter}
+import cn.graiph.regionfs.shell.{ShellFileSystemStarter, ShellNodeServerStarter}
 
 /**
   * Created by bluejoe on 2019/8/31.
   */
 object StartSingleTestServer {
   def main(args: Array[String]) {
-    //example args: ./node1.conf
+    ShellFileSystemStarter.main(Array("./regionfs.conf"));
+    ShellNodeServerStarter.main(Array("./node1.conf"))
+  }
+}
+
+object StartAnyTestServer {
+  //args: ./node1.conf
+  def main(args: Array[String]) {
     ShellNodeServerStarter.main(args)
   }
 }

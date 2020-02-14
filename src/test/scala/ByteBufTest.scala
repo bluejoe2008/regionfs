f6a8a980d81789db0b1a2040df1b1052db8819a7
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2020-02-14 21:29:46 +0800

    best score: 188ms

--- src/test/scala/ByteBufTest.scala
+++ src/test/scala/ByteBufTest.scala
@@ -1,15 +1,21 @@
 import java.nio.ByteBuffer
 
+import cn.graiph.regionfs.util.Profiler
 import io.netty.buffer.Unpooled
 import org.junit.Test
+import cn.graiph.regionfs.util.Profiler._
 
 /**
   * Created by bluejoe on 2020/2/14.
   */
 class ByteBufTest {
+  Profiler.enableTiming = true
+
   @Test
   def testByteBuffer(): Unit = {
-    val bb = ByteBuffer.allocate(20)
+    val bb = timing(true){
+      ByteBuffer.allocate(20)
+    }
     println(bb.array(), bb.arrayOffset(), bb.position(), bb.remaining())
     bb.putInt(100)
     println(bb.array(), bb.arrayOffset(), bb.position(), bb.remaining())
@@ -20,7 +26,9 @@ class ByteBufTest {
   
   @Test
   def testByteBuf(): Unit = {
-    val bb = Unpooled.buffer(10)
+    val bb = timing(true){
+      Unpooled.buffer(10)
+    }
     println(bb.array(), bb.arrayOffset(), bb.readerIndex(), bb.writerIndex(), bb.readableBytes())
 
     bb.writeInt(100)

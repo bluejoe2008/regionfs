80df929dc4a7c646bd0e5452817bc71c2590cb02
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2020-03-09 18:00:21 +0800

    multinode test

--- src/test/scala/regionfs/ShellCmdTest.scala
+++ src/test/scala/regionfs/ShellCmdTest.scala
@@ -12,10 +12,5 @@ class ShellCmdTest extends FileTestBase with MultiNode {
   def testStat(): Unit = {
     RegionFSCmd.main("put -zk localhost:2181 ./pom.xml ./README.md".split(" "));
     RegionFSCmd.main("stat-all -zk localhost:2181".split(" "));
-    /*
-    RegionFSCmd.main("get -zk localhost:2181 AAAAAAABAAEAAAAAAAAjKg== AAAAAAABAAEAAAAAAAAjKw==".split(" "));
-    RegionFSCmd.main("get -zk localhost:2181 -dir ./testdata AAAAAAABAAEAAAAAAAAjKg== AAAAAAABAAEAAAAAAAAjKw==".split(" "));
-    RegionFSCmd.main("get -zk localhost:2181 FFAAAAABAAEAAAAAAAAjKgBA".split(" "));
-    */
   }
 }

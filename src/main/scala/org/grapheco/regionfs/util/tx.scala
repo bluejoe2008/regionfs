f1da15bbace60b0a484d6b8a1c9ca70de0bf86cb
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2020-04-03 10:40:01 +0800

    tx with retry

--- src/main/scala/org/grapheco/regionfs/util/tx.scala
+++ src/main/scala/org/grapheco/regionfs/util/tx.scala
@@ -1,15 +1,20 @@
 package org.grapheco.regionfs.util
 
+import java.util.concurrent.atomic.AtomicLong
+
 /**
   * Created by bluejoe on 2020/4/2.
   */
 object Transactional {
+  val idgen = new AtomicLong(0)
+
   def apply[X, Y](f: (X) => Rollbackable[Y]) = new Transactional[X, Y] {
     def perform(x: X, ctx: TransactionalContext): Rollbackable[Y] = f(x)
   }
 
-  def run[X, Y](tx: Transactional[X, Y], x: X): Y = {
-    val r = tx.perform(x, new TransactionalContext() {})
+  def run[X, Y](tx: Transactional[X, Y], x: X, retryStrategy: RetryStrategy = RetryStrategy.RUN_ONCE): Y = {
+    val ctx = new TransactionalContext(idgen.incrementAndGet(), retryStrategy)
+    val r = ctx.runWithRetry(tx.perform(_: X, ctx), x)
     r match {
       case Success(result: Y, _) =>
         result
@@ -19,8 +24,43 @@ object Transactional {
   }
 }
 
-trait TransactionalContext {
+private class TransactionalContext(transactionId: Long, retryStrategy: RetryStrategy) {
+  def runWithRetry[X, Y](f: (X) => Rollbackable[Y], x: X): Rollbackable[Y] = retryStrategy.runWithRetry(f, x)
+}
 
+object RetryStrategy {
+  val RUN_ONCE = new RetryStrategy() {
+    def runWithRetry[X, Y](f: (X) => Rollbackable[Y], x: X): Rollbackable[Y] = {
+      f(x)
+    }
+  }
+
+  def FOR_TIMES(n: Int) = new RetryStrategy() {
+    def runWithRetry[X, Y](f: (X) => Rollbackable[Y], x: X): Rollbackable[Y] = {
+      var r = null.asInstanceOf[Rollbackable[Y]]
+      var i = 0
+      do {
+        r = f(x)
+        i += 1
+      } while (r.isInstanceOf[Failure[_]] && i < n)
+      r
+    }
+  }
+
+  def WITHIN_TIME(ms: Int) = new RetryStrategy() {
+    def runWithRetry[X, Y](f: (X) => Rollbackable[Y], x: X): Rollbackable[Y] = {
+      var r = null.asInstanceOf[Rollbackable[Y]]
+      val timeout = System.currentTimeMillis() + ms
+      do {
+        r = f(x)
+      } while (r.isInstanceOf[Failure[_]] && System.currentTimeMillis() < timeout)
+      r
+    }
+  }
+}
+
+trait RetryStrategy {
+  def runWithRetry[X, Y](f: (X) => Rollbackable[Y], x: X): Rollbackable[Y]
 }
 
 trait Transactional[X, Y] {
@@ -30,10 +70,10 @@ trait Transactional[X, Y] {
     val predecessor = this
     new Transactional[X, Z] {
       def perform(x: X, ctx: TransactionalContext): Rollbackable[Z] = {
-        val r1 = predecessor.perform(x, ctx)
+        val r1 = ctx.runWithRetry(predecessor.perform(_: X, ctx), x)
         r1 match {
           case Success(y: Y, rollback) =>
-            val r2 = f(y)
+            val r2 = ctx.runWithRetry(f, y)
             r2 match {
               case Success(z: Z, rollback2) =>
                 Rollbackable.success(z) {
--- src/test/scala/regionfs/TxTest.scala
+++ src/test/scala/regionfs/TxTest.scala
@@ -1,57 +1,72 @@
 package regionfs
 
-import org.grapheco.regionfs.util.{Rollbackable, TransactionFailedException, Transactional}
-import org.junit.{Assert, Test}
+import org.grapheco.regionfs.util.{RetryStrategy, Rollbackable, TransactionFailedException, Transactional}
+import org.junit.{Assert, Before, Test}
 
 /**
   * Created by bluejoe on 2020/4/2.
   */
 class TxTest {
-  @Test
-  def test1(): Unit = {
+  var (bluejoe, jason) = (100, 250)
 
-    var (bluejoe, jason) = (100, 250)
+  //jason pay money to bluejoe
+  def step1(money: Int): Rollbackable[Int] = {
+    println(s"run step1...")
 
-    //jason pay money to bluejoe
-    val tx = Transactional[Int, Int] {
-      //step1
-      (money: Int) => {
-        val ov = jason
-        if (jason < money) {
-          Rollbackable.failure(new Exception("jason has no sufficient money!"))
-        }
-        else {
-          jason -= money
-          Rollbackable.success(money) {
-            jason = ov
-          }
-        }
-      }
-    }.then[Boolean] {
-      //step2
-      (money: Int) => {
-        val ov = bluejoe
-        bluejoe += money
-        if (bluejoe > 300) {
-          bluejoe = ov
-          Rollbackable.failure(new Exception("too much money for bluejoe!"))
-        }
-        else {
-          Rollbackable.success(true) {
-            bluejoe = ov
-          }
-        }
+    val ov = jason
+    if (jason < money) {
+      Rollbackable.failure(new Exception("jason has no sufficient money!"))
+    }
+    else {
+      jason -= money
+      Rollbackable.success(money) {
+        jason = ov
       }
     }
+  }
 
+  def step2(money: Int): Rollbackable[Boolean] = {
+    println(s"run step2...")
+
+    val ov = bluejoe
+    bluejoe += money
+    if (bluejoe > 300) {
+      bluejoe = ov
+      Rollbackable.failure(new Exception("too much money for bluejoe!"))
+    }
+    else {
+      Rollbackable.success(true) {
+        bluejoe = ov
+      }
+    }
+  }
+
+  val tx = Transactional[Int, Int] {
+    (money: Int) => {
+      step1(money)
+    }
+  }.then[Boolean] {
+    (money: Int) => {
+      step2(money)
+    }
+  }
+
+  @Before
+  def reset(): Unit = {
+    bluejoe = 100
+    jason = 250
+  }
+
+  @Test
+  def testNormal(): Unit = {
     val i = Transactional.run(tx, 50)
     Assert.assertEquals(true, i)
     Assert.assertEquals(150, bluejoe)
     Assert.assertEquals(200, jason)
+  }
 
-    bluejoe = 100
-    jason = 250
-
+  @Test
+  def testStep1Failed(): Unit = {
     //fails on 1-st step
     try {
       val i2 = Transactional.run(tx, 300)
@@ -67,7 +82,10 @@ class TxTest {
 
     //value should be rolled back
     Assert.assertEquals((100, 250), (bluejoe, jason))
+  }
 
+  @Test
+  def testStep2Failed(): Unit = {
     //fails on 2-st step
     try {
       val i2 = Transactional.run(tx, 220)
@@ -83,5 +101,92 @@ class TxTest {
 
     //value should be rolled back
     Assert.assertEquals((100, 250), (bluejoe, jason))
+  }
+
+  @Test
+  def testRetry(): Unit = {
+    var counter = 0
+    //fails on step3
+    val tx2 = tx.then[String]((x: Boolean) => {
+      counter += 1
+      println(s"run step3...")
+      if (counter < 5)
+        Rollbackable.failure(new Exception("retry for success"))
+      else
+        Rollbackable.success("OK") {}
+    })
+
+    try {
+      Transactional.run(tx2, 50, RetryStrategy.RUN_ONCE)
+      Assert.assertTrue(false)
+    }
+    catch {
+      case e: TransactionFailedException =>
+        Assert.assertTrue(true)
+      case e =>
+        Assert.assertTrue(false)
+    }
+
+    val r2 = Transactional.run(tx2, 50, RetryStrategy.FOR_TIMES(5))
+    Assert.assertEquals("OK", r2)
+    Assert.assertEquals(counter, 5)
+  }
+
+  @Test
+  def testRetry2(): Unit = {
+    var counter = 0
+    //fails on step0
+    val tx2 = Transactional[Int, Int]((m: Int) => {
+      counter += 1
+      println(s"run step0...")
+      if (counter < 5)
+        Rollbackable.failure(new Exception("retry for success"))
+      else
+        Rollbackable.success(m) {}
+    }).then(step1(_)).then(step2(_))
+
+    try {
+      Transactional.run(tx2, 50, RetryStrategy.RUN_ONCE)
+      Assert.assertTrue(false)
+    }
+    catch {
+      case e: TransactionFailedException =>
+        Assert.assertTrue(true)
+      case e =>
+        Assert.assertTrue(false)
+    }
+
+    val r2 = Transactional.run(tx2, 50, RetryStrategy.FOR_TIMES(5))
+    Assert.assertEquals(true, r2)
+    Assert.assertEquals(counter, 5)
+  }
+
+  @Test
+  def testRetry3(): Unit = {
+    var counter = 0
+    //fails on step1.5
+    val tx2 = Transactional[Int, Int](step1(_)).then[Int]((m: Int) => {
+      counter += 1
+      println(s"run step1.5...")
+      if (counter < 5)
+        Rollbackable.failure(new Exception("retry for success"))
+      else
+        Rollbackable.success(m) {}
+    }).then(step2(_))
+
+    try {
+      Transactional.run(tx2, 50, RetryStrategy.RUN_ONCE)
+      Assert.assertTrue(false)
+    }
+    catch {
+      case e: TransactionFailedException =>
+        Assert.assertTrue(true)
+      case e =>
+        Assert.assertTrue(false)
+    }
+
+    val r2 = Transactional.run(tx2, 50, RetryStrategy.FOR_TIMES(5))
+    Assert.assertEquals(true, r2)
+    Assert.assertEquals(counter, 5)
   }
 }
